331025ce76de1c0cb14c12efa424831c9d780196
jmcopier <jessecopier2003@outlook.com>
2025-02-06 13:41:10 +0100

    Database migrations

---
+++ database/migrations/2025_02_06_093518_create_experiences_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('profile_id')->constrained();
+            $table->string('function');
+            $table->string('company');
+            $table->string('period');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
